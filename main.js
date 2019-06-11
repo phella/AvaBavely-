d8f134fd28dc383cf3281338d14fdce062a23a50
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-06-11 18:07:58 +0200

    add google maps

--- main.js
+++ main.js
@@ -15,24 +15,30 @@ function scrolling(){
 function navColor() {
         if(scrollState >= 10){
         document.getElementById("nav").style.backgroundColor = "white";
-        for(let i=1;i<4;i++){
+        for(let i=1;i<6;i++){
             document.getElementById("nav-elements"+i).style.color = "black";
         }
     } else {
         document.getElementById("nav").style.backgroundColor = "transparent";
-        for(let i=1;i<4;i++){
+        for(let i=1;i<6;i++){
             document.getElementById("nav-elements"+i).style.color = "white";
         }
     }
 }
 
 function animate() {
-    if(scrollState>320) {
-        document.getElementById("fig1").style.transitionDuration = "3s";
-        document.getElementById("fig1").style.transform = "translateY(-150px)";
-        document.getElementById("fig1").style.opacity ="1";
-        document.getElementById("arti1").style.transitionDuration = "2s";
-        document.getElementById("arti1").style.transform = "translateY(-150px)";
-        document.getElementById("arti1").style.opacity ="1";
+    if(scrollState>320 && scrollState<380) {
+        translate(1);
+    }else if (scrollState>380) {
+        translate(2);
     }
+}
+
+function translate(i){
+    document.getElementById("fig"+i).style.transitionDuration = "3s";
+    document.getElementById("fig"+i).style.transform = "translateY(-150px)";
+    document.getElementById("fig"+i).style.opacity ="1";
+    document.getElementById("arti"+i).style.transitionDuration = "2s";
+    document.getElementById("arti"+i).style.transform = "translateY(-150px)";
+    document.getElementById("arti"+i).style.opacity ="1";
 }
