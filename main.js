0da16acc726c3e6b090829d7e1a770cabf29e15c
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-06-11 16:01:10 +0200

    fade in animation

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ let scrollState;
 let maxScroll=0;
 function scrolling(){
     scrollState = window.scrollY;
+    console.log(scrollState);
+    if(scrollState>maxScroll)
+    {
+        animate();
+    }
     if(scrollState > maxScroll){
         maxScroll = scrollState;
     }
     navColor();
 }
-function navColor(){
+function navColor() {
         if(scrollState >= 10){
         document.getElementById("nav").style.backgroundColor = "white";
         for(let i=1;i<4;i++){
@@ -19,5 +24,16 @@ function navColor(){
         for(let i=1;i<4;i++){
             document.getElementById("nav-elements"+i).style.color = "white";
         }
+    }
+}
+
+function animate() {
+    if(scrollState>320) {
+        document.getElementById("fig1").style.transitionDuration = "3s";
+        document.getElementById("fig1").style.transform = "translateY(-150px)";
+        document.getElementById("fig1").style.opacity ="1";
+        document.getElementById("arti1").style.transitionDuration = "2s";
+        document.getElementById("arti1").style.transform = "translateY(-150px)";
+        document.getElementById("arti1").style.opacity ="1";
     }
 }
