eb621ae10dee90c779e8f2dc44b5d8ded21bc112
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-06-11 06:56:07 +0200

    first section template

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function animate(){
-        if(window.scrollY >= 10){
+let scrollState;
+let maxScroll=0;
+function scrolling(){
+    scrollState = window.scrollY;
+    if(scrollState > maxScroll){
+        maxScroll = scrollState;
+    }
+    navColor();
+}
+function navColor(){
+        if(scrollState >= 10){
         document.getElementById("nav").style.backgroundColor = "white";
         for(let i=1;i<4;i++){
             document.getElementById("nav-elements"+i).style.color = "black";
         }
+        document.getElementById("nav-elements3").style.color = "blue";
     } else {
         document.getElementById("nav").style.backgroundColor = "transparent";
         for(let i=1;i<4;i++){
@@ -11,9 +21,3 @@ function animate(){
         }
     }
 }
-
-window.addEventListener("scroll", function (event) {
-    var scroll = this.scrollY;
-    console.log(scroll);
-    console.log(this);
-});
