e163fc28172be79d858f21bd9eb5bb9c5ec92a66
philopateer <philopateer.elmaseh98@eng-st.cu.edu.eg>
2019-06-18 19:47:28 +0200

    colapse navbar

--- main.js
+++ main.js
@@ -66,12 +66,12 @@ function scrolling(){
 function navColor() {
         if(scrollState >= 10){
         document.getElementById("nav").style.backgroundColor = "white";
-        for(let i=1;i<5;i++){
+        for(let i=1;i<6;i++){
             document.getElementById("nav-elements"+i).style.color = "black";
         }
     } else {
         document.getElementById("nav").style.backgroundColor = "transparent";
-        for(let i=1;i<5;i++){
+        for(let i=1;i<6;i++){
             document.getElementById("nav-elements"+i).style.color = "white";
         }
     }
